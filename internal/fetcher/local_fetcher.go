9f6d886fb5e24159a5bcf3c492a453b2580e74f5
sushichan044 <71284054+sushichan044@users.noreply.github.com>
2025-05-17 15:18:24 +0900

    refactor: PackageFetcher follows new config format

--- internal/fetcher/local_fetcher.go
+++ internal/fetcher/local_fetcher.go
@@ -6,24 +6,25 @@ import (
 
 	cp "github.com/otiai10/copy"
 
+	"github.com/sushichan044/ajisai/internal/config"
 	"github.com/sushichan044/ajisai/internal/domain"
 	"github.com/sushichan044/ajisai/utils"
 )
 
 type LocalFetcher struct{}
 
-func NewLocalFetcher() domain.ContentFetcher {
+func NewLocalFetcher() domain.PackageFetcher {
 	return &LocalFetcher{}
 }
 
 // Fetch copies content from the source local directory (defined in source.Details)
 // to the destinationDir.
 // It expects source.Details to be of type domain.LocalInputSourceDetails.
-func (f *LocalFetcher) Fetch(source domain.InputSource, destinationDir string) error {
-	localDetails, ok := domain.GetInputSourceDetails[domain.LocalInputSourceDetails](source)
+func (f *LocalFetcher) Fetch(source config.ImportedPackage, destinationDir string) error {
+	localDetails, ok := config.GetImportDetails[config.LocalImportDetails](source)
 	if !ok {
 		return &InvalidSourceTypeError{
-			expectedType: domain.PresetSourceTypeLocal,
+			expectedType: config.ImportTypeLocal,
 			actualType:   source.Type,
 			err:          fmt.Errorf("cannot fetch from source type: %s", source.Type),
 		}
