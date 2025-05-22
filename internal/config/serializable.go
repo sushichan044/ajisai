343751f75c7fe846186f6ee7053fff65fda5900d
sushichan044 <71284054+sushichan044@users.noreply.github.com>
2025-05-23 02:31:56 +0900

    chore: lint

--- internal/config/serializable.go
+++ internal/config/serializable.go
@@ -1,6 +1,9 @@
 package config
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 type (
 	SerializableConfig struct {
@@ -63,178 +66,233 @@ func NewSerializer() configSerializer {
 	return &configSerializerImpl{}
 }
 
+type recoverableNilInputError struct {
+}
+
+func (e *recoverableNilInputError) Error() string {
+	return "sentinel: this is valid early return"
+}
+
+func (e *recoverableNilInputError) Unwrap() error {
+	return nil
+}
+
 func (s *configSerializerImpl) Serialize(cfg *Config) (SerializableConfig, error) {
-	var serializableCfg SerializableConfig
+	serializableSettings := serializeSettings(cfg.Settings)
+	serializablePackage := serializePackage(cfg.Package)
+	serializableWorkspace, err := serializeWorkspace(cfg.Workspace)
 
-	if cfg.Settings != nil {
-		var settings serializableSettings = serializableSettings{
-			CacheDir:     cfg.Settings.CacheDir,
-			Experimental: cfg.Settings.Experimental,
-			Namespace:    cfg.Settings.Namespace,
-		}
-		serializableCfg.Settings = &settings
+	if err != nil && !errors.Is(err, &recoverableNilInputError{}) {
+		return SerializableConfig{}, err
 	}
 
-	if cfg.Package != nil {
-		var pkg serializablePackage
-		pkg.Name = cfg.Package.Name
-
-		if cfg.Package.Exports != nil {
-			pkg.Exports = make(map[string]serializableExportedPresetDefinition, len(cfg.Package.Exports))
-
-			for name, export := range cfg.Package.Exports {
-				pkg.Exports[name] = serializableExportedPresetDefinition(export)
-			}
-		}
-		serializableCfg.Package = &pkg
-	}
-
-	if cfg.Workspace != nil {
-		var workspace serializableWorkspace
-		workspace.Imports = make(map[string]serializableImportedPackage, len(cfg.Workspace.Imports))
-
-		for name, imp := range cfg.Workspace.Imports {
-			switch imp.Type {
-			case ImportTypeLocal:
-				if details, ok := GetImportDetails[LocalImportDetails](imp); ok {
-					workspace.Imports[name] = serializableImportedPackage{
-						Type:    string(imp.Type),
-						Path:    details.Path,
-						Include: imp.Include,
-					}
-				}
-			case ImportTypeGit:
-				if details, ok := GetImportDetails[GitImportDetails](imp); ok {
-					workspace.Imports[name] = serializableImportedPackage{
-						Type:       string(imp.Type),
-						Repository: details.Repository,
-						Revision:   details.Revision,
-						Include:    imp.Include,
-					}
-				}
-			default:
-				continue
-				// TODO: Gracefully handling unsupported import type
-				// return SerializableConfig{}, fmt.Errorf("unsupported import type: %s", imp.Type)
-			}
-		}
-
-		if cfg.Workspace.Integrations != nil {
-			var integrations serializableAgentIntegration
-
-			if cfg.Workspace.Integrations.Cursor != nil {
-				var cursor serializableCursorIntegration = serializableCursorIntegration{
-					Enabled: cfg.Workspace.Integrations.Cursor.Enabled,
-				}
-				integrations.Cursor = &cursor
-			}
-
-			if cfg.Workspace.Integrations.GitHubCopilot != nil {
-				var githubCopilot serializableGitHubCopilotIntegration = serializableGitHubCopilotIntegration{
-					Enabled: cfg.Workspace.Integrations.GitHubCopilot.Enabled,
-				}
-				integrations.GitHubCopilot = &githubCopilot
-			}
-
-			if cfg.Workspace.Integrations.Windsurf != nil {
-				var windsurf serializableWindsurfIntegration = serializableWindsurfIntegration{
-					Enabled: cfg.Workspace.Integrations.Windsurf.Enabled,
-				}
-				integrations.Windsurf = &windsurf
-			}
-
-			workspace.Integrations = &integrations
-		}
-
-		serializableCfg.Workspace = &workspace
-	}
-
-	return serializableCfg, nil
+	return SerializableConfig{
+		Settings:  serializableSettings,
+		Package:   serializablePackage,
+		Workspace: serializableWorkspace,
+	}, nil
 }
 
 func (s *configSerializerImpl) Deserialize(cfg SerializableConfig) (*Config, error) {
+	settings := deserializeSettings(cfg.Settings)
+	pkg := deserializePackage(cfg.Package)
+	workspace, err := deserializeWorkspace(cfg.Workspace)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Config{
+		Settings:  settings,
+		Package:   pkg,
+		Workspace: workspace,
+	}, nil
+}
+
+func serializeSettings(settings *Settings) *serializableSettings {
+	if settings == nil {
+		return nil
+	}
+
+	return &serializableSettings{
+		CacheDir:     settings.CacheDir,
+		Experimental: settings.Experimental,
+		Namespace:    settings.Namespace,
+	}
+}
+
+func deserializeSettings(serializableSettings *serializableSettings) *Settings {
 	var settings Settings
-	if cfg.Settings != nil {
-		settings.CacheDir = cfg.Settings.CacheDir
-		settings.Experimental = cfg.Settings.Experimental
-		settings.Namespace = cfg.Settings.Namespace
+
+	if serializableSettings == nil {
+		return &settings
 	}
 
-	var workspace Workspace
-	if cfg.Workspace != nil {
-		imports := make(map[string]ImportedPackage, len(cfg.Workspace.Imports))
+	settings.CacheDir = serializableSettings.CacheDir
+	settings.Experimental = serializableSettings.Experimental
+	settings.Namespace = serializableSettings.Namespace
 
-		for name, imp := range cfg.Workspace.Imports {
-			switch ImportType(imp.Type) {
-			case ImportTypeLocal:
-				imports[name] = ImportedPackage{
-					Type: ImportTypeLocal,
-					Details: LocalImportDetails{
-						Path: imp.Path,
-					},
-					Include: imp.Include,
-				}
-				continue
-			case ImportTypeGit:
-				imports[name] = ImportedPackage{
-					Type: ImportTypeGit,
-					Details: GitImportDetails{
-						Repository: imp.Repository,
-						Revision:   imp.Revision,
-					},
-					Include: imp.Include,
-				}
-				continue
-			}
+	return &settings
+}
 
-			return nil, fmt.Errorf("unsupported import type: %s", imp.Type)
-		}
-		workspace.Imports = imports
-
-		var integrations AgentIntegrations
-		if cfg.Workspace.Integrations != nil {
-			integrations = AgentIntegrations{}
-
-			var cursor CursorIntegration
-			if cfg.Workspace.Integrations.Cursor != nil {
-				cursor = CursorIntegration{Enabled: cfg.Workspace.Integrations.Cursor.Enabled}
-			}
-			integrations.Cursor = &cursor
-
-			var githubCopilot GitHubCopilotIntegration
-			if cfg.Workspace.Integrations.GitHubCopilot != nil {
-				githubCopilot = GitHubCopilotIntegration{
-					Enabled: cfg.Workspace.Integrations.GitHubCopilot.Enabled,
-				}
-			}
-			integrations.GitHubCopilot = &githubCopilot
-
-			var windsurf WindsurfIntegration
-			if cfg.Workspace.Integrations.Windsurf != nil {
-				windsurf = WindsurfIntegration{
-					Enabled: cfg.Workspace.Integrations.Windsurf.Enabled,
-				}
-			}
-			integrations.Windsurf = &windsurf
-		}
-		workspace.Integrations = &integrations
+func serializePackage(pkg *Package) *serializablePackage {
+	if pkg == nil {
+		return nil
 	}
 
+	var s serializablePackage
+	s.Name = pkg.Name
+
+	if pkg.Exports != nil {
+		s.Exports = make(map[string]serializableExportedPresetDefinition, len(pkg.Exports))
+
+		for name, export := range pkg.Exports {
+			s.Exports[name] = serializableExportedPresetDefinition(export)
+		}
+	}
+
+	return &s
+}
+
+func deserializePackage(sPkg *serializablePackage) *Package {
 	var pkg Package
-	if cfg.Package != nil {
-		pkg.Name = cfg.Package.Name
-		if cfg.Package.Exports != nil {
-			pkg.Exports = make(map[string]ExportedPresetDefinition, len(cfg.Package.Exports))
+	if sPkg != nil {
+		pkg.Name = sPkg.Name
+		if sPkg.Exports != nil {
+			pkg.Exports = make(map[string]ExportedPresetDefinition, len(sPkg.Exports))
 
-			for name, export := range cfg.Package.Exports {
+			for name, export := range sPkg.Exports {
 				pkg.Exports[name] = ExportedPresetDefinition(export)
 			}
 		}
 	}
 
-	return &Config{
-		Settings:  &settings,
-		Package:   &pkg,
-		Workspace: &workspace,
-	}, nil
+	return &pkg
+}
+
+func serializeWorkspace(workspace *Workspace) (*serializableWorkspace, error) {
+	if workspace == nil {
+		// sentinel: this is valid early return
+		return nil, &recoverableNilInputError{}
+	}
+
+	var s serializableWorkspace
+	s.Imports = make(map[string]serializableImportedPackage, len(workspace.Imports))
+
+	for name, imp := range workspace.Imports {
+		switch imp.Type {
+		case ImportTypeLocal:
+			if details, ok := GetImportDetails[LocalImportDetails](imp); ok {
+				s.Imports[name] = serializableImportedPackage{
+					Type:    string(imp.Type),
+					Path:    details.Path,
+					Include: imp.Include,
+				}
+			}
+		case ImportTypeGit:
+			if details, ok := GetImportDetails[GitImportDetails](imp); ok {
+				s.Imports[name] = serializableImportedPackage{
+					Type:       string(imp.Type),
+					Repository: details.Repository,
+					Revision:   details.Revision,
+					Include:    imp.Include,
+				}
+			}
+		default:
+			return nil, fmt.Errorf("unsupported import type: %s", imp.Type)
+		}
+	}
+
+	if workspace.Integrations != nil {
+		var integrations serializableAgentIntegration
+
+		if workspace.Integrations.Cursor != nil {
+			var cursor = serializableCursorIntegration{
+				Enabled: workspace.Integrations.Cursor.Enabled,
+			}
+			integrations.Cursor = &cursor
+		}
+
+		if workspace.Integrations.GitHubCopilot != nil {
+			var githubCopilot = serializableGitHubCopilotIntegration{
+				Enabled: workspace.Integrations.GitHubCopilot.Enabled,
+			}
+			integrations.GitHubCopilot = &githubCopilot
+		}
+
+		if workspace.Integrations.Windsurf != nil {
+			var windsurf = serializableWindsurfIntegration{
+				Enabled: workspace.Integrations.Windsurf.Enabled,
+			}
+			integrations.Windsurf = &windsurf
+		}
+
+		s.Integrations = &integrations
+	}
+
+	return &s, nil
+}
+
+func deserializeWorkspace(sWorkspace *serializableWorkspace) (*Workspace, error) {
+	var workspace Workspace
+	if sWorkspace == nil {
+		return &workspace, nil
+	}
+
+	imports := make(map[string]ImportedPackage, len(sWorkspace.Imports))
+	for name, imp := range sWorkspace.Imports {
+		switch ImportType(imp.Type) {
+		case ImportTypeLocal:
+			imports[name] = ImportedPackage{
+				Type: ImportTypeLocal,
+				Details: LocalImportDetails{
+					Path: imp.Path,
+				},
+				Include: imp.Include,
+			}
+			continue
+		case ImportTypeGit:
+			imports[name] = ImportedPackage{
+				Type: ImportTypeGit,
+				Details: GitImportDetails{
+					Repository: imp.Repository,
+					Revision:   imp.Revision,
+				},
+				Include: imp.Include,
+			}
+			continue
+		}
+
+		return nil, fmt.Errorf("unsupported import type: %s", imp.Type)
+	}
+	workspace.Imports = imports
+
+	var integrations AgentIntegrations
+	if sWorkspace.Integrations != nil {
+		integrations = AgentIntegrations{}
+
+		var cursor CursorIntegration
+		if sWorkspace.Integrations.Cursor != nil {
+			cursor = CursorIntegration{Enabled: sWorkspace.Integrations.Cursor.Enabled}
+		}
+		integrations.Cursor = &cursor
+
+		var githubCopilot GitHubCopilotIntegration
+		if sWorkspace.Integrations.GitHubCopilot != nil {
+			githubCopilot = GitHubCopilotIntegration{
+				Enabled: sWorkspace.Integrations.GitHubCopilot.Enabled,
+			}
+		}
+		integrations.GitHubCopilot = &githubCopilot
+
+		var windsurf WindsurfIntegration
+		if sWorkspace.Integrations.Windsurf != nil {
+			windsurf = WindsurfIntegration{
+				Enabled: sWorkspace.Integrations.Windsurf.Enabled,
+			}
+		}
+		integrations.Windsurf = &windsurf
+	}
+	workspace.Integrations = &integrations
+
+	return &workspace, nil
 }
